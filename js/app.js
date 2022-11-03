d0361f67627d1ef5956162003d2abcb4c37051be
Roberto Siqueira <72571916+robertosiqueirajr@users.noreply.github.com>
2022-11-03 17:45:06 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,38 @@
+function fnAction(defineDate) {
+
+    defineDate =  document.getElementById('date').value;
+
+    const URL = `https://api.nasa.gov/planetary/apod?api_key=m4pzhqgNPPeya6TpMSf1D1hJb9gh1Ga4WNLU1YJS&date=${defineDate}&concept_tags=True`;
+
+    fetch(URL)
+    .then((readURL) => readURL.json())
+    .then((readJson) => {
+
+        // console.log(readJson.url)
+
+        const result = document.getElementById('root')
+        result.innerHTML = `
+
+      <div class="card mb-3" style="max-width: 1200px;">
+        <div class="row g-0">
+          <div class="col-md-6">
+            <img src="${readJson.hdurl}" class="img-fluid rounded-start" alt="${readJson.explanation}">
+          </div>
+          <div class="col-lg-6">
+            <div class="card-body">
+              <h5 class="card-title">${readJson.title}</h5>
+              <p class="card-text">${readJson.explanation}</p>
+              <p class="card-text"><small class="text-muted">Copyright: ${readJson.copyright} | Date Image: ${readJson.date}</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+            
+          `;
+    })
+
+    .catch((error) => console.log(error));
+
+}
+
+fnAction();
